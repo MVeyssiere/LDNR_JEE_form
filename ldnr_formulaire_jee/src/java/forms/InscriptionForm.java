46e306fdefa5e781942584472cec3dd686563e0a
stagldnr <stagldnr@debian-java-ldnr.ldnr.local>
2019-05-09 13:42:04 +0200

    include methode to validate mail, password and name. (move from inscription.java)

--- ldnr_formulaire_jee/src/java/forms/InscriptionForm.java
+++ ldnr_formulaire_jee/src/java/forms/InscriptionForm.java
@@ -1,5 +1,6 @@
 package forms;
 
+import beans.User;
 import java.util.HashMap;
 import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
@@ -35,12 +36,78 @@ public final class InscriptionForm {
         }
     }
 
+    // champ email valide
+    private void validateEmail(String email) throws Exception {
+        if (!email.isEmpty()) {  //si la case mail est remplie
+            //si mail non valide
+            if (!email.matches("([^.@]+)(\\.[^.@]+)*@([^.@]+\\.)+([^.@]+)")) {
+                throw new Exception("Le format de l'email n'est pas valide.");
+            }
+        } else {
+            throw new Exception("Merci de saisir une adresse mail.");
+        }
+    }
+
+    // password et confirm ont 3 lettres au moins et sont identiques
+    private void validatePassword(String password, String confirm) throws Exception {
+        if (!password.isEmpty() && !confirm.isEmpty()) {
+            if (!password.equals(confirm)) {
+                throw new Exception("Les mots de passe entrés sont différents. Merci de les saisir à nouveau.");
+            } else {
+                if (!password.matches("^.{3,}$")) {
+                    throw new Exception("Votre mot de passe doit contenir au moins 3 lettres.");
+                }
+            }
+        } else {
+            if (password.isEmpty()) {
+                throw new Exception("Merci de saisir un mot de passe");
+            }
+            if (confirm.isEmpty()) {
+                throw new Exception("Veuillez confirmer votre mot de passe.");
+            }
+        }
+    }
+
+    // champ facultatif name >2 caractères
+    private void validateName(String name) throws Exception {
+        if (!name.isEmpty() && !name.matches("^[a-zA-Z]{3,}")) {
+            throw new Exception("Le nom d'utilisateur doit contenir au moins 3 caractères.");
+        }
+        if (name.isEmpty()) {
+            throw new Exception("Merci de saisir un nom d'utilisateur.");
+        }
+    }
+
+
     public void inscribeUser(HttpServletRequest request) {
         /* Récupération des champs du formulaire. */
         String mail = getParamValue(request, EMAIL);
         String pass = getParamValue(request, PWD);
         String confirm = getParamValue(request, CONFIRM);
         String name = getParamValue(request, USERNAME);
+
+        User user = new User();
+        // setting of values of the bean object user
+        user.setEmail(EMAIL);
+        user.setName(name);
+        user.setPassword(pass);
+
+        try {
+            validateEmail(mail);
+        } catch (Exception ex) {
+//                Logger.getLogger(Inscription.class.getName()).log(Level.SEVERE, null, ex);
+            errors.put(EMAIL, ex.getMessage());
+        }
+        try {
+            validatePassword(pass, confirm);
+        } catch (Exception ex) {
+            errors.put(PWD, ex.getMessage());
+        }
+        try {
+            validateName(name);
+        } catch (Exception ex) {
+            errors.put(USERNAME, ex.getMessage());
+        }
 
     }
 
